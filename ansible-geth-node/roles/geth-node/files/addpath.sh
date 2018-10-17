ac157f0551bb2ec12398c8c567cd0a09a069fd31
Branislav <brani.djuric@gmail.com>
2018-10-17 02:09:29 +0200

    setup geth rpc at 8545 ansible

---
+++ ansible-geth-node/roles/geth-node/files/addpath.sh
@@ -0,0 +1,7 @@
+ 	#!/bin/bash
+
+# Add a PATH if it isn't already there
+if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
+    PATH="${PATH:+"$PATH:"}$1"
+    echo "export PATH=${PATH:+"$PATH:"}$1" >> ~/.bash_profile
+fi
